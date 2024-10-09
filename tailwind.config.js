b086bfcf52d10828f3d5929ac33f93d7d687421a
fcoPhox <fcophox@MacBook-Pro-de-fcoPhox-2.local>
2024-10-08 21:28:15 -0300

    Primer commit: Proyecto React inicial

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // Asegúrate de incluir tus archivos de React aquí
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
